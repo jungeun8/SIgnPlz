706a0bd15e818506267f5192309b0ea861dd9187
seoinhyuk <sih2341@gmail.com>
2021-05-31 15:17:19 +0900

    ADD 스웨거 예시 추가

--- signPlz/src/main/java/signPlz/controller/UserController.java
+++ signPlz/src/main/java/signPlz/controller/UserController.java
@@ -1,14 +1,18 @@
 package signPlz.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import signPlz.model.User.Register;
 import signPlz.model.User.UserLogin;
 import signPlz.service.UserService;
 
+@Api(tags = { "001. User" })
 @RestController
 public class UserController {
 
@@ -20,7 +24,7 @@ public class UserController {
 		return userService.loginCheck(param);
 	}
 	
-	//회원가입하기 
+	@ApiOperation(value = "회원가입", notes = "회원가입")
 	@PostMapping("/user")
 	public void insertUsesr(@RequestBody Register register){
 		userService.insertUser(register);
